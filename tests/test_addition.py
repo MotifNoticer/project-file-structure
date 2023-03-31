b2e98e42f05376510085090d1b537cb8ec7612cd
Kaliane <vankaliane@gmail.com>
2023-03-31 14:53:16 -0400

    "changed tests"

--- tests/test_addition.py
+++ tests/test_addition.py
@@ -2,6 +2,6 @@ import src.addition as addition
 
 def test_addition():
     # Assert
-    assert perform_operation(1, 1) == 2
-    assert perform_operation(800, 88) == 888
-    assert perform_operation(-1, 1) == 0
+    assert addition.perform_operation(1, 1) == 2
+    assert addition.perform_operation(800, 88) == 888
+    assert addition.perform_operation(-1, 1) == 0
--- tests/test_division.py
+++ tests/test_division.py
@@ -1,7 +1,7 @@
 import src.division as divison
 
 def test_division():
-    assert perform_operation(10, 5) == 2
-    assert perform_operation(1, 1) == 1
-    assert perform_operation(100, 2) == 50
+    assert division.perform_operation(10, 5) == 2
+    assert division.perform_operation(1, 1) == 1
+    assert division.perform_operation(100, 2) == 50
 
